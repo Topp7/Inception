aa3b394efad7253ae4751062b8291f4c87e0de0a
Soren Topp <stopp@2-F-2.42heilbronn.de>
2025-03-14 16:49:07 +0100

    fixed and tested mariadb, set up basics for nginx (not tested)

--- src/requirements/mariadb/tools/mariadb_setup.sh
+++ src/requirements/mariadb/tools/mariadb_setup.sh
@@ -12,26 +12,28 @@ fi
 
 mysqld_safe --user=mysql &
 
-until mysqladmin -u root ping -h localhost --silent;
-do	echo -e "Waiting for MariaDB setup."
-	sleep 2
+until mysqladmin -u root ping -h localhost --silent; do
+		echo -e "Waiting for MariaDB setup."
+		sleep 2
 done
 
 echo -e "MariaDB started!"
 
 #Changing PW
-TEMP_PASS=$(grep 'temporary password' "/var/log/mysql.log" | awk '{print $NF}')
-mysql -u root -p"${TEMP_PASS}" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB-ROOT-PW}';"
+TEMP_PASS=$(grep 'temporary password' "/var/log/mysql/error.log" | awk '{print $NF}')
+mysql -u root -p"${TEMP_PASS}" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PW}';"
 
 #DataBase check
-DATABASE=$(mysql -u root -p${DB-ROOT-PW} -e "SHOW DATABASE LIKE '${DB-NAME}';" | grep "${DB-NAME}")
+DATABASE=$(mysql -u root -p${DB_ROOT_PW} -e "SHOW DATABASES LIKE '${DB_NAME}';" | grep "${DB_NAME}")
 if [ -z "$DATABASE" ];
 then	echo -e "Creating Database."
-		mysql -u root -p{DB-ROOT-PW} -e "CREATE DATABASE ${DB-NAME};"
-		mysql -u root -p{DB-ROOT-PW} -e "CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB-USER-PW}';"
-		mysql -u root -p{DB-ROOT-PW} -e "GRANT ALL PRIVILEGES ON ${DB-NAME}.* TO '${DB-USER}'@'%';"
-		mysql -u root -p{DB-ROOT-PW} -e "FLUSH PRIVILEGES;"
+		mysql -u root -p${DB_ROOT_PW} -e "CREATE DATABASE ${DB_NAME};"
+		mysql -u root -p${DB_ROOT_PW} -e "CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_USER_PW}';"
+		mysql -u root -p${DB_ROOT_PW} -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';"
+		mysql -u root -p${DB_ROOT_PW} -e "FLUSH PRIVILEGES;"
 fi
 
 echo -e "MariaDB setup done"
 
+mysqladmin -u root -p${DB_ROOT_PW} shutdown
+exec mysqld_safe --user=mysql
